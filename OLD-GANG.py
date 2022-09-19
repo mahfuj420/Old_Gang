4660416b483ec9972812dacdeb36902ff4ebc241
N.A.M Mahfuj <64296613+mahfuj420@users.noreply.github.com>
2022-09-20 01:45:26 +0600

    Update OLD-GANG.py

--- OLD-GANG.py
+++ OLD-GANG.py
@@ -1362,7 +1362,7 @@ def Subscraption():
 		print("")
 		print(" 𝑪𝒐𝒑𝒚 𝑨𝒏𝒅 𝑷𝒂𝒔𝒕𝒆 𝑲𝒆𝒚 𝑰𝒏𝒃𝒐𝒙")
 		print ("")
-		print (" Your Key : "+ak+OldGang+key1)
+		print (" Your Key : "+old+Gang+key1)
 		print ("")
 		name = input(" Your Name : ")
 		print ("")
